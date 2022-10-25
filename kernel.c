a6cf3684a9abcafd38fb8598b91c77beea5f3e98
David <drochon59@yahoo.com>
2022-10-25 12:51:10 -0400

    Project A

---
+++ kernel.c
@@ -0,0 +1,19 @@
+//kernel.c
+
+void main(){
+	int startVidMem = 0xb800;
+	int vidMemOffset = 0x0;
+	int white = 0x7;
+	char* letters = "Hello World\0";
+
+	while(*letters != 0x0) {
+		putInMemory(startVidMem, vidMemOffset, *letters);
+		vidMemOffset++;
+		putInMemory(startVidMem, vidMemOffset, white);
+		vidMemOffset++;
+		//avance letters pointer
+		letters++;
+	}
+
+	while(1);
+}
